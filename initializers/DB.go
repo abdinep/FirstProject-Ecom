e63c5f7027e232abe583c808d8e0896473cf6163
abdin <abdineppanambra@gmail.com>
2024-08-10 11:21:55 +0530

    db.go changes

--- initializers/DB.go
+++ initializers/DB.go
@@ -13,8 +13,13 @@ var DB *gorm.DB
 
 func Dbinit() {
 	var err error
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 
-	dsn := os.Getenv("DSN")
+	dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPassword + " dbname=" + dbName + " port=" + dbPort + " sslmode=disable TimeZone=Asia/Shanghai"
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect DB")
